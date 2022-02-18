2c2f1fd4377682b8efc9b1d456b04cfcec815c6d
Irfan <46717573+IrfanEi@users.noreply.github.com>
2022-02-18 17:29:19 +0700

    Initial commit

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+Use App\mahasiswa;
+
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        return mahasiswa::all();
+    }
+
+    public function show(mahasiswa $mahasiswa)
+    {
+        return $mahasiswa;
+    }
+
+    public function store(Request $request)
+    {
+        $mahasiswa = mahasiswa::create($request->all());
+
+        return response()->json($mahasiswa, 201);
+    }
+
+    public function update(Request $request, mahasiswa $mahasiswa)
+    {
+        $mahasiswa->update($request->all());
+
+        return response()->json($mahasiswa, 200);
+    }
+
+    public function delete(mahasiswa $mahasiswa)
+    {
+        $mahasiswa->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+Use App\mahasiswa;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+ 
+Route::get('mahasiswa', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return mahasiswa::all();
+});
+ 
+Route::get('mahasiswa/{NIP}', function($NIP) {
+    return mahasiswa::find($NIP);
+});
+
+Route::post('mahasiswa', function(Request $request) {
+    return mahasiswa::create($request->all);
+});
+
+Route::put('mahasiswa/{NIP}', function(Request $request, $NIP) {
+    $mahasiswa = mahasiswa::findOrFail($NIP);
+    $mahasiswa->update($request->all());
+
+    return $mahasiswa;
+});
+
+Route::delete('mahasiswa/{NIP}', function($NIP) {
+    mahasiswa::find($NIP)->delete();
+
+    return 204;
+});
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+Route::get('mahasiswa', 'MahasiswaController@index');
+Route::get('mahasiswa/{mahasiswa}', 'MahasiswaController@show');
+Route::post('mahasiswa', 'MahasiswaController@store');
+Route::put('mahasiswa/{mahasiswa}', 'MahasiswaController@update');
+Route::delete('mahasiswa/{mahasiswa}', 'MahasiswaController@delete');
