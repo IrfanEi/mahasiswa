723227fa986717ceda5657c2b00a4d73636321f3
Irfan <46717573+IrfanEi@users.noreply.github.com>
2022-02-21 17:18:42 +0700

    Enter\

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -8,34 +8,64 @@ use App\Models\mahasiswa;
 
 class MahasiswaController extends Controller
 {
-    public function index(Request $request)
-    {
-        return mahasiswa::all();
+    public function index(Request $request) {
+        $mahasiswa = mahasiswa::all();
+        return response()->json(["result" => $mahasiswa], 200);
     }
 
-    public function show(mahasiswa $mahasiswa)
-    {
-        return $mahasiswa;
+    public function store(Request $request) {
+        $mahasiswa = new mahasiswa;
+
+        $mahasiswa->name = $request->name;
+        $mahasiswa->nip = $request->nip;
+        $mahasiswa->address = $request->address;
+        $mahasiswa->registrationDate = $request->registrationDate;
+
+        $mahasiswa->save();
+
+        return response()->json(["result" => "ok"], 201);
     }
 
-    public function store(Request $request)
-    {
-        $mahasiswa = mahasiswa::create($request->all());
-
-        return response()->json($mahasiswa, 201);
+    public function show(Request $request, $mahasiswaId) {
+        $mahasiswa = mahasiswa::find($mahasiswaId);
+        return response()->json(["result" => $mahasiswa], 200);
     }
 
-    public function update(Request $request, mahasiswa $mahasiswa)
-    {
-        $mahasiswa->update($request->all());
-
-        return response()->json($mahasiswa, 200);
+    public function search(Request $request, $nip) {
+        $mahasiswa = mahasiswa::where('nip', '=', $nip)->first();
+        return response()->json(["result" => $mahasiswa], 200);
     }
 
-    public function delete(mahasiswa $mahasiswa)
+    /*public function update(Request $request, $studentId)
     {
-        $mahasiswa->delete();
+        $student = Student::find($studentId);
 
-        return response()->json(null, 204);
+        if (!is_null($request->name)){
+            $student->name = $request->name;
+        }
+
+        if (!is_null($request->nip)){
+            $student->nip = $request->nip;
+        }
+
+        if (!is_null($request->address)) {
+            $student->address = $request->address;
+        }
+
+        if (!is_null($request->registrationDate)){
+            $student->registrationDate = $request->registrationDate;
+        }
+
+        $student->save();
+
+        return response()->json(["result" => $student], 200);       
     }
+
+    public function destroy($studentId)
+    {
+        $student = Student::find($studentId);
+        $student->delete();
+
+        return response()->json(["result" => "ok"], 200);       
+    }*/
 }
